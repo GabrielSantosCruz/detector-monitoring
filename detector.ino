6f1716fcdf54f0f67aa1aa027e1a343f7e158c79
Gabriel Santos <gabrielsantoscruz61@gmail.com>
2025-04-24 08:37:37 -0300

    :sparkles: feat(SD Card adapter): conexão com o adaptador de cartão

--- detector.ino
+++ detector.ino
@@ -1,15 +1,18 @@
 #include <Wire.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <SD.h>
+#include <SPI.h>
 #include "RTClib.h"
 
 // #define BOTAO 8                                     // Pino do botão
 #define SERVER_URL "http://192.168.198.114:3000/dados"  // URL do servidor
 #define GEIGER_PIN 0                                    // Pino do detector
+#define CS_PIN     20
 
 // dados da rede
-const char* ssid = "Casa_2";
-const char* password = "gami2022";
+const char* ssid = "UEFS_VISITANTES";
+const char* password = "";
 
 RTC_DS3231 rtc;                                    // Objeto do RTC
 volatile unsigned long contadorHoras[24] = { 0 };  // Vetor para contar apertos por hora
@@ -22,7 +25,7 @@ void IRAM_ATTR contarPulso() {
 }
 
 // Função para enviar os dados via HTTP POST
-void enviarDados() {
+void sendData() {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
     http.begin(SERVER_URL);
@@ -53,6 +56,24 @@ void enviarDados() {
   }
 }
 
+// Função para salvar os dados no cartão SD
+void writeData() {
+  File arquivo = SD.open("/dadosDetector.txt", FILE_APPEND);
+  if (arquivo) {
+    arquivo.print("[");
+    for(int i = 0; i < 24; i++){
+      arquivo.print(contadorHoras[i]);
+      if(i < 23){
+        arquivo.print(", ");
+      }
+    }
+    arquivo.print("]");
+    arquivo.close();
+  } else {
+    Serial.println("Erro ao abrir o arquivo");
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -77,7 +98,7 @@ void setup() {
   Wire.beginTransmission(0x68);  // Endereço do DS3231
   Wire.write(0x0E);              // Seleciona o registrador de controle
   Wire.write(0b00011100);        // Habilita saída de onda quadrada e ativa o oscilador
-  Wire.endTransmission();
+  Wire.endTransmission();        // finazlia a comunicação
 
   if (rtc.lostPower()) {
     Serial.println("⚠ O RTC perdeu energia! Ajustando o horário...");
@@ -86,35 +107,28 @@ void setup() {
     Serial.println("✅ RTC funcionando corretamente.");
   }
 
-  // pinMode(BOTAO, INPUT_PULLUP);  // Configura o botão com pull-up interno
   pinMode(GEIGER_PIN, INPUT);  // Configura o pino como entrada
+
   // Configura interrupção na borda de descida
   attachInterrupt(digitalPinToInterrupt(GEIGER_PIN), contarPulso, FALLING);
+
+  Serial.println("Inicializando cartão SD...");
+
+  if (!SD.begin(CS_PIN)) {
+    Serial.println("⚠ Falha ao inicializar o cartão SD!");
+    return;
+  }
+  Serial.println("✅ Cartão SD inicializado com sucesso!");
 }
 
 void loop() {
   DateTime agora = rtc.now();    // Obtém a hora atual do DS3231
   minutoAtual = agora.minute();  // Pega apenas o minuto
 
-  // //Verifica se o botão foi pressionado
-  // if (digitalRead(BOTAO) == LOW && !botaoPressionado) {
-  //   contadorHoras[minutoAtual]++;  // Incrementa a posição correspondente ao minuto
-  //   botaoPressionado = true;
-  //   Serial.print("Botão pressionado! Minuto: ");
-  //   Serial.println(minutoAtual);
-  //   enviarDados();
-  // }
-
-  // // Reseta a flag quando o botão for solto
-  // if (digitalRead(BOTAO) == HIGH) {
-  //   botaoPressionado = false;
-  // }
-
-  // A cada 3 segundos, envia os dados
   static unsigned long ultimoEnvio = 0;
-  if (millis() - ultimoEnvio > 3000) {
-    enviarDados();
+  if (millis() - ultimoEnvio > 6000) {
+    sendData();
+    writeData();
     ultimoEnvio = millis();
   }
 }
-
